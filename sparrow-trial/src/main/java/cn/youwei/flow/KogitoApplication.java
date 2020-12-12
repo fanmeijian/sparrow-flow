13102fd1a72e204a2a11eae99a98fca2ab35c50b
fanmeijian <fanmeijian@aliyun.com>
2020-12-12 12:33:43 +0800

    将流程服务注册为微服务，支持eureka

--- sparrow-trial/src/main/java/cn/youwei/flow/KogitoApplication.java
+++ sparrow-trial/src/main/java/cn/youwei/flow/KogitoApplication.java
@@ -2,7 +2,9 @@ package cn.youwei.flow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication(scanBasePackages={"cn.youwei.flow.**", "org.kie.kogito.**"})
 public class KogitoApplication {
 
